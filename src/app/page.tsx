927969879d6d1c604988453207ac2a6e28587c5a
EHTISHAM-Afzal <khaj3172@gmail.com>
2024-08-16 15:16:43 +0500

    Changes to be committed: 	modified:   src/app/mdx-route/page.mdx 	modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import { ModeToggle } from "@/components/ui/ModeToggle";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
+      <Link href="/mdx-route" className="mb-20">
+      Article
+      </Link>
       <h1 className="pb-3 text-4xl font-semibold tracking-tight text-center scroll-m-20 lg:text-6xl">
         Create T3 Template <br />
         By Ehtisham
